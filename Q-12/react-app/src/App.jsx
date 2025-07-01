4cb5c2a21b3135ab84dda59cb1e7223b6d627540
Himanshu kumar <himansuraj01081999@gmail.in>
2025-07-01 13:16:59 +0530

    c

---
+++ Q-12/react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <ul>
+        <li>React</li>
+        <li>JavaScript</li>
+        <li>CSS</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
